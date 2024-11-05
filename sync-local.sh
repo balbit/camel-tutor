f8a7780947a17e9e9a6f4ebbfa8db8f4b1a9415e
balbit <balbit@mit.edu>
2024-11-05 00:51:11 -0500

    local dev stuff

--- sync-local.sh
+++ sync-local.sh
@@ -5,6 +5,9 @@ npx tsc
 
 # Sync JavaScript files to NGINX static file directory
 sudo cp dist/src/ts/*.js /usr/local/var/www/js/
+sudo cp src/css/* /usr/local/var/www/js/
+sudo cp src/assets/* /usr/local/var/www/js/
+sudo cp src/html/* /usr/local/var/www/
 
 # Sync NGINX configuration
 sudo cp nginx.conf /opt/homebrew/etc/nginx/nginx.conf
